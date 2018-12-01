62fa13daee38775ed2574bfb286644178aa7e2c5
Samed Duzcay <samedduzcay@gmail.com>
2018-12-01 18:56:12 +0300

    Add the migration for School:Kid many to many relation

---
+++ db/migrate/20181201152928_create_schools_kids_join_table.rb
@@ -0,0 +1,8 @@
+class CreateSchoolsKidsJoinTable < ActiveRecord::Migration[5.2]
+  def change
+    create_join_table :schools, :kids do |t|
+      t.index :school_id
+      t.index :kid_id
+    end
+  end
+end
