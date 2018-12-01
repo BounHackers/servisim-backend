797fc31458e317760e334a71593619942d7c6ac3
Samed Duzcay <samedduzcay@gmail.com>
2018-12-01 22:30:37 +0300

    Fix rubocop errors, finally

--- m.rb
+++ m.rb
@@ -1,9 +1,10 @@
-require 'oauth2'
-client = OAuth2::Client.new('client_id', 'client_secret', :site => 'https://api.secure.mercedes-benz.com/oidc10/')
+# frozen_string_literal: true
 
-client.auth_code.authorize_url(:redirect_uri => 'http://localhost:8080/oauth2/callback')
+require 'oauth2'
+client = OAuth2::Client.new('client_id', 'client_secret', site: 'https://api.secure.mercedes-benz.com/oidc10/auth')
+client.auth_code.authorize_url(redirect_uri: 'http://localhost:3000/api/v1/mercedes/oauth2/callback')
 # => "https://example.org/oauth/authorization?response_type=code&client_id=client_id&redirect_uri=http://localhost:8080/oauth2/callback"
 
-token = client.auth_code.get_token('authorization_code_value', :redirect_uri => 'http://localhost:8080/oauth2/callback', :headers => {'Authorization' => 'Basic some_password'})
-response = token.get('/api/resource', :params => { 'query_foo' => 'bar' })
+token = client.auth_code.get_token('authorization_code_value', redirect_uri: 'http://localhost:3000/api/v1/mercedes/oauth2/callback')
+response = token.get('/api/resource', params: { 'query_foo' => 'bar' })
 response.class.name
