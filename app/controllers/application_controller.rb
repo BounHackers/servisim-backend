5c9428cd6b762c96ea95cc4d90f38d2735ee3dfe
Samed Duzcay <samedduzcay@gmail.com>
2018-12-01 19:01:55 +0300

    Minor syntax fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::API
   def current_user
     if session[:user_id]
-      if session[:user_type] == 'parent' do
+      if session[:user_type] == 'parent'
         @current_user ||= Parent.find_by_id session[:user_id]
-      elsif session[:user_type] == 'kid' do
+      elsif session[:user_type] == 'kid'
         @current_user ||= Kid.find_by_id session[:user_id]
-      elsif session[:user_type] == 'driver' do
+      elsif session[:user_type] == 'driver'
         @current_user ||= Driver.find_by_id session[:user_id]
       else
         @current_user = nil
